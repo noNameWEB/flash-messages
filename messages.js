ba15b84eb210b3c38b2290165dc606a26031464c
surtic86 <surtic86@gmail.com>
2015-11-14 00:33:33 +0100

    js beautify, semantic-ui changes, added message title

--- messages.js
+++ messages.js
@@ -1,5 +1,5 @@
 /**
- * flashMessages 
+ * flashMessages
  * { message: String,
  *   style: String,
  *   seen: Boolean }
@@ -7,40 +7,43 @@
 flashMessages = new Mongo.Collection(null);
 
 FlashMessages = {
-  // Deprecated, use sendWarning instead. sendWarning is more consistent with Boostrap classes.
-  sendAlert: function(message, options) {
-    sendMessage(message, '', options);
-    console.log('Deprecated, use sendWarning instead of sendAlert');
-  },
-  sendWarning: function(message, options) {
-    sendMessage(message, 'alert-warning', options);
-  },
-  sendError: function(message, options) {
-    sendMessage(message, 'alert-error alert-danger', options);
-  },
-  sendSuccess: function(message, options) {
-    sendMessage(message, 'alert-success', options);
-  },
-  sendInfo: function(message, options) {
-    sendMessage(message, 'alert-info', options);
-  },
-  clear: function() {
-    flashMessages.remove({seen: true});
-  },
-  configure: function(options) {
-    this.options = this.options || {};
-    _.extend(this.options, options);
-  },
-  options: {
-    autoHide: true,
-    hideDelay: 5000,
-    autoScroll: true
-  }
-}
+    sendWarning: function(message, title, options) {
+        sendMessage(message, title, 'alert-warning', options);
+    },
+    sendError: function(message, title, options) {
+        sendMessage(message, title, 'alert-error alert-danger', options);
+    },
+    sendSuccess: function(message, title, options) {
+        sendMessage(message, title, 'alert-success', options);
+    },
+    sendInfo: function(message, title, options) {
+        sendMessage(message, title, 'alert-info', options);
+    },
+    clear: function() {
+        flashMessages.remove({
+            seen: true
+        });
+    },
+    configure: function(options) {
+        this.options = this.options || {};
+        _.extend(this.options, options);
+    },
+    options: {
+        autoHide: true,
+        hideDelay: 5000,
+        autoScroll: true
+    }
+};
 
-sendMessage = function(message, style, options) {
-  options = options || {};
-  options.autoHide = options.autoHide === undefined ? FlashMessages.options.autoHide : options.autoHide;
-  options.hideDelay = options.hideDelay || FlashMessages.options.hideDelay;
-  flashMessages.insert({ message: message, style: style, seen: false, options: options});  
-}
+sendMessage = function(message, title, style, options) {
+    options = options || {};
+    options.autoHide = options.autoHide === undefined ? FlashMessages.options.autoHide : options.autoHide;
+    options.hideDelay = options.hideDelay || FlashMessages.options.hideDelay;
+    flashMessages.insert({
+        message: message,
+        title: title,
+        style: style,
+        seen: false,
+        options: options
+    });
+};
--- messages_list.js
+++ messages_list.js
@@ -1,36 +1,42 @@
 Template.flashMessages.helpers({
-  messages: function () {
-    if (flashMessages.find().count() && FlashMessages.options.autoScroll)
-      $('html, body').animate({
-        scrollTop: 0
-      }, 200);
-    var messages = flashMessages.find().fetch();
-    $.each(messages, function(index, value) {
-      value.group = value.message instanceof Array;
-    });
-    return messages;
-  }
+    messages: function() {
+        if (flashMessages.find().count() && FlashMessages.options.autoScroll)
+            $('html, body').animate({
+                scrollTop: 0
+            }, 200);
+        var messages = flashMessages.find().fetch();
+        $.each(messages, function(index, value) {
+            value.group = value.message instanceof Array;
+        });
+        return messages;
+    }
 });
 
-Template.flashMessageItem.rendered = function () {
-  var message = this.data;
-  Meteor.defer(function() {
-    flashMessages.update(message._id, {$set: {seen: true}});
-  });
-  if (message.options && message.options.autoHide) {
-    var $alert = $(this.find('.alert'));
-    Meteor.setTimeout(function() {
-        $alert.fadeOut(400, function() {
-          flashMessages.remove({_id: message._id});
-        });    
-      }, 
-      message.options.hideDelay);
-  }
+Template.flashMessageItem.rendered = function() {
+    var message = this.data;
+    Meteor.defer(function() {
+        flashMessages.update(message._id, {
+            $set: {
+                seen: true
+            }
+        });
+    });
+    if (message.options && message.options.autoHide) {
+        var $alert = $(this.find('.alert'));
+        Meteor.setTimeout(function() {
+                $alert.fadeOut(400, function() {
+                    flashMessages.remove({
+                        _id: message._id
+                    });
+                });
+            },
+            message.options.hideDelay);
+    }
 };
 
 Template.flashMessageItem.events({
-  "click .close": function (e, tmpl) {
-    e.preventDefault();
-    flashMessages.remove(tmpl.data._id);
-  }
+    "click .close": function(e, tmpl) {
+        e.preventDefault();
+        flashMessages.remove(tmpl.data._id);
+    }
 });
